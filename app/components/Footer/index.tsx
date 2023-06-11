db2849aac2c15b5198ab8342be3a706d29479d93
daniArbesu <daniel.arbesu.sanchez@gmail.com>
2023-06-11 16:43:54 +0200

    feat: add footer

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -1,5 +1,19 @@
+import { bodoni_moda } from '@/app/utils/fonts';
+import SectionHeader from '../UI/SectionHeader';
+import styles from './styles.module.css';
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <section className={styles.footer}>
+      <SectionHeader title="Made In" />
+      <h2
+        className={`${styles.location} ${bodoni_moda.className}`}
+        id="location-text"
+      >
+        Valencia, Spain
+      </h2>
+    </section>
+  );
 };
 
 export default Footer;
